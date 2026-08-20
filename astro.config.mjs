42973b86358254f65b50889958210908d55935e1
coffinn <coffin2105@gmail.com>
2026-08-20 15:08:20 -0500

    testing deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
+
 import pagefind from 'astro-pagefind';
 import { webcore } from 'webcoreui/integration';
 
 const isVercel = process.env.VERCEL === '1';
 
 export default defineConfig({
-  site:
-    isVercel && process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : 'https://akhalaplo.github.io',
-  base: isVercel ? '/' : '/genshin-builds',
-  integrations: [webcore(), pagefind()],
+    site:
+        isVercel && process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : 'https://coffinn.github.io',
+
+    base: isVercel ? '/' : '/star-rail-builds',
+
+    integrations: [webcore(), pagefind()],
 });
