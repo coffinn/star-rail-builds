7b864e2134b805b1102aa5ffb64788323c03742f
Dem6667 <florino252rf@gmail.com>
2026-08-26 12:00:58 +0300

    enabled ru language

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -1,10 +1,10 @@
 export const languages = [
     { code: 'en', name: 'English' },
+    { code: 'ru', name: 'Русский' },
     /*{ code: 'fr', name: 'Français' },
     { code: 'de', name: 'Deutsch' },
     { code: 'es', name: 'Español' },
     { code: 'it', name: 'Italiano' },
-    { code: 'ru', name: 'Русский' },
     { code: 'pt', name: 'Português' },*/
 ] as const;
 
